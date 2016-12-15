811cee9d5510b75ede130f86e6cc25ae69e71093
Jeremie Dimino <jdimino@janestreet.com>
2016-12-15 11:20:46 +0000

    114.20+69

--- install.ml
+++
@@ -1,9 +0,0 @@
-#use "topfind";;
-#require "js-build-tools.oasis2opam_install";;
-
-open Oasis2opam_install;;
-
-generate ~package:"ppx_conv_func"
-  [ oasis_lib "ppx_conv_func"
-  ; file "META" ~section:"lib"
-  ]
